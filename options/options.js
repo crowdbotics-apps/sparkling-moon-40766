7f16cfa3ab03b6cd1c430780462d36a8277f626c
Crowdbotics <team@crowdbotics.com>
2023-05-17 11:30:46 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "sparkling-moon-40766",
+  url: "https://sparkling-moon-40766.botics.co",
+  api: "https://sparkling-moon-40766.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
